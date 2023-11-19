60211426fcb629ffda8dfc5ee31751a7be101438
CristianRdz <20213tn086@utez.edu.mx>
2023-11-19 13:47:11 -0600

    Add STORAGE_URL constant

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,9 @@
 // Obtenemos la url de la api
 const API_URL = "http://192.168.1.71:8080"
+
+const STORAGE_URL = `${API_URL}/api/product/loadfile/`
 // exportamos las constantes
 export {
     API_URL,
+    STORAGE_URL
 }
