ca44b1d91cb635d99f79aa0c65bd0a01f3f5f28e
CristianRdz <20213tn086@utez.edu.mx>
2023-12-10 23:37:25 -0600

    Refactor price display in Products and Cart components

--- src/components/admin/products/Products.js
+++ src/components/admin/products/Products.js
@@ -42,15 +42,41 @@ export default function Products({ productos, fetchDataOut }) {
         >
           {producto.name}
         </Text>
-        <Text
-          style={{
-            fontSize: textSizes.Small,
-            fontWeight: "bold",
-            color: colors.tertiary,
-          }}
-        >
-          ${producto.price} MXN
-        </Text>
+
+        <View style={styles.preciosContainer}>
+          {producto.priceDiscount > 0 && (
+          <>
+            <Text
+            style={{
+              ...styles.priceText,
+              color: colors.primary,
+              fontSize: textSizes.Text,
+            }}
+          >
+            {"$ " + producto.priceDiscount + " MXN"}
+          </Text>
+            <Text
+              style={{
+                ...styles.precioAnterior,
+                color: colors.tertiary,
+                fontSize: textSizes.Text,
+              }}
+            >
+              {"$ " + producto.price + " MXN"}
+            </Text>
+            </>
+          ) || (
+            <Text
+            style={{
+              ...styles.priceText,
+              color: colors.primary,
+              fontSize: textSizes.Text,
+            }}
+          >
+            {"$ " + producto.price + " MXN"}
+          </Text>
+          )}
+        </View>
       </View>
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={styles.iconContainer}>
@@ -222,6 +248,22 @@ const styles = StyleSheet.create({
     width: "95%",
     alignSelf: "center",
     borderRadius: 10,
+  },
+
+  priceText: {
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  precioAnterior: {
+    fontWeight: "bold",
+    textDecorationLine: "line-through",
+    marginBottom: 10,
+    marginLeft: 5,
+  },
+  preciosContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    marginBottom: 5,
   },
 
   buttonContainer: {
